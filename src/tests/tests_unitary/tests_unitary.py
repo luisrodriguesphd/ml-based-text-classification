54273b67c92b813e2cd236ef94a41b6f7624346f
Luis Rodrigues <larpig@gmail.com>
2023-09-17 13:08:04 +0100

    test: add a sample of unit tests

---
+++ src/tests/tests_unitary/tests_unitary.py
@@ -0,0 +1,20 @@
+"""Module for unit testing"""
+
+import pytest
+
+from text_classification.pipelines.training.nodes import (
+    train_model,
+)
+
+from .unitary_test_data import (
+    data_test_train_model,
+)
+
+@pytest.mark.parametrize(argnames="test_data", argvalues=data_test_train_model)
+def test_train_model(test_data):
+    """unit test for train_model function"""
+    params = {"model_training": {"vectorizer_parameters": {}, "classifier_parameters": {}}}
+    vec, clf = train_model(test_data["X"], test_data["y"], params)
+
+    assert vec.__class__.__name__ == test_data["vec_name"]
+    assert clf.__class__.__name__ == test_data["clf_name"]
---
+++ src/tests/tests_unitary/unitary_test_data.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+from text_classification.utils.data_prep import (
+    text_cleaner,
+)
+
+# Load text dataset 
+n_samples = 600
+newsgroups = fetch_20newsgroups(
+    subset = "test",
+    categories=["rec.sport.baseball", "rec.sport.hockey", 'talk.politics.guns'],
+    shuffle=True,
+    random_state=0,
+    remove=("headers", "footers", "quotes"),
+    return_X_y=False,
+)
+X = text_cleaner(np.array(newsgroups.data[:n_samples]))
+y = np.array(newsgroups.target[:n_samples])
+labels = np.unique(y)
+label_encoder = dict(zip([str(label) for label in labels], range(len(labels))))
+y_encoded = np.array([label_encoder[str(yi)] for yi in y])
+
+
+data_test_train_model = [
+    {
+        "X": X,
+        "y": y_encoded,
+        "vec_name": "TfidfVectorizer",
+        "clf_name": "XGBClassifier",
+    }
+]
