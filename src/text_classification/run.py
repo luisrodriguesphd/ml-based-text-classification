a4c1d855b61bf2db98be1619ad4a070e0648721d
Luis Rodrigues <larpig@gmail.com>
2023-09-18 20:41:48 +0100

    dev: da

--- src/text_classification/run.py
+++ src/text_classification/run.py
@@ -1,3 +1,5 @@
+"""https://docs.kedro.org/en/0.16.2/04_user_guide/03_configuration.html#local-and-base-configuration"""
+
 from kedro.framework.context import KedroContext
 import os
 from kedro.framework.project import settings
@@ -5,6 +7,8 @@ from kedro.framework.project import settings
 src_path = os.path.dirname(os.path.dirname(__file__))
 project_path = os.path.dirname(src_path)
 conf_path = os.path.join(project_path, settings.CONF_SOURCE)
+
+conf_path = os.path.join("text-classification", "text-classification", "conf")
 
 class ProjectContext(KedroContext):
     CONF_ROOT = conf_path
